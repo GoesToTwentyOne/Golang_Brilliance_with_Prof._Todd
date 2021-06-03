daa6b42df338f48c56d9ecf16da9ee8ca6a69aee
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-03 21:34:30 +0600

    var spec

---
+++ codesamples/04_var_keyword_little_bit_scope_and_difference_of_var_and_short_declaration/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// declare and assign = intializition
+
+var y = 45
+
+func main() {
+	x := 21
+	fmt.Println(x)
+	fmt.Println(y)
+	goes()
+}
+func goes() {
+	fmt.Println(y)
+}
