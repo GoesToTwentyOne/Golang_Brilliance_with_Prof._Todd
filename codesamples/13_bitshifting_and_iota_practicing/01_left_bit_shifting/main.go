d4ce645aced6f6885bbb1f59569536c19e7ba28d
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-08 21:04:22 +0600

    left and right bit shifting

---
+++ codesamples/13_bitshifting_and_iota_practicing/01_left_bit_shifting/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 1
+	leftshifting := 1 << 10
+	b := 100
+	rightshipt := 1000 >> 2
+	fmt.Printf("decimal  a value  %d \n", a)
+	fmt.Printf("bainary a value %b \n", a)
+	fmt.Printf("decimal  a leftshifrting value  %d \n", leftshifting)
+	fmt.Printf("bainary a leftshifrting value %b \n", leftshifting)
+	fmt.Printf("decimal  a value  %d \n", b)
+	fmt.Printf("bainary a value %b \n", b)
+	fmt.Printf("decimal  a rightshipt value  %d \n", rightshipt)
+	fmt.Printf("bainary a rightshipt value %b \n", rightshipt)
+}
