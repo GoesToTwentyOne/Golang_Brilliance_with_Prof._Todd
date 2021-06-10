526c82f6719ea65a74cbdbe9f6b8738aa374dbbd
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-10 12:32:07 +0600

    complete handon exercise

---
+++ codesamples/17_control_flow_hands_on_erercise/07/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "Tony"
+	if a == "Anny" {
+		fmt.Println("match")
+	} else if a == "Many" {
+		fmt.Println("match")
+	} else if a == "Tony" {
+		fmt.Println("match")
+	} else {
+		fmt.Println("match")
+	}
+
+}
