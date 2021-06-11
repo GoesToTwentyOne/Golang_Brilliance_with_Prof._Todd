fa176ce3996685983d45ab01719e355bd91d0a16
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-11 23:05:01 +0600

    create my own delete func  tricky process

--- codesamples/18_goruping_data/09_slice_delete_RemoveIndex_func/main.go
+++ codesamples/18_goruping_data/09_slice_delete_RemoveIndex_func/main.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	x := []int{1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 44, 88, 66}
 	fmt.Println(x)
-	x = RemoveIndex(x, 4)
+	x = RemoveIndexPositionValue(x, 4)
 	fmt.Println(x)
 
 }
-func RemoveIndex(s []int, index int) []int {
+func RemoveIndexPositionValue(s []int, index int) []int {
 	return append(s[:index], s[index+1:]...)
 }
