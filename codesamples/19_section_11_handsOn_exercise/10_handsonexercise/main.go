726a95d2912acdd6b75b67a66582a4ea28ffc814
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-12 13:20:12 +0600

    handson complete

---
+++ codesamples/19_section_11_handsOn_exercise/10_handsonexercise/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	x[`Nadidolapo`] = []string{`Manjaro,Turusko`, `Guava`, `Beach love`}
+	//before delete
+	fmt.Println("before delete ", x)
+
+	//delete
+	delete(x, `moneypenny_miss`)
+
+	for key, rowvalue := range x {
+		fmt.Println("record name ", key)
+		for i, colvalue := range rowvalue {
+			fmt.Printf("\t \t index position %v  value %v\n", i, colvalue)
+		}
+
+	}
+	fmt.Println("after delete ", x)
+
+}
