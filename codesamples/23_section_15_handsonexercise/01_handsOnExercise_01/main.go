7c7f945acce8d5cc84fd099d57fe22572e995968
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-16 12:02:30 +0600

    handsonerxercise 1

---
+++ codesamples/23_section_15_handsonexercise/01_handsOnExercise_01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(foo())
+	fmt.Println(bar())
+
+}
+func foo() int {
+	return 4545
+
+}
+func bar() (string, int) {
+	return "Hello From Bangladesg", 454545
+
+}
