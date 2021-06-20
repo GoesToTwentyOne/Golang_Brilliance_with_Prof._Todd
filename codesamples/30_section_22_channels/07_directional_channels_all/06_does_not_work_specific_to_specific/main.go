9ab771b642933b74b0038c7f010664828649ab94
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-21 02:52:32 +0600

    does not work specific to specific

---
+++ codesamples/30_section_22_channels/07_directional_channels_all/06_does_not_work_specific_to_specific/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+	chr := make(<-chan int) //received
+	chs := make(chan<- int) //send
+	fmt.Printf("%T \n", ch)
+	fmt.Printf("%T \n", chr)
+	fmt.Printf("%T \n", chs)
+	// specific to specific doesn't assign
+	// chr= chs
+}
