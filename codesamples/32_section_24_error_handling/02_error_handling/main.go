0c3c398ad3a7e669136b04f53650c9db197732db
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-24 01:59:29 +0600

    error handling

---
+++ codesamples/32_section_24_error_handling/02_error_handling/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var ans1, ans2, ans3 string
+	fmt.Print("Name :")
+	_, err := fmt.Scan(&ans1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("FavCodeingLang :")
+	_, err = fmt.Scan(&ans2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("FavDrinks :")
+	_, err = fmt.Scan(&ans3)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(ans1, ans2, ans3)
+
+}
