4b8d1370de510cc76c7952f7a2bc08d0a8550c0b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-27 00:34:17 +0600

    go vet

---
+++ codesamples/35_section_28_testing_and_benchmarking/04_testing/main.go
@@ -0,0 +1,10 @@
+package main
+
+// Prints out "Super Mario 3"
+func main() {
+	// game_version := 3
+	// fmt.Printf("Super Mario %s\n", game_version)
+
+	//command go vet main.go
+	// show the report
+}
