777e5f8d17c9c1dfa22b8130ae1a4740cb369135
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-27 21:44:52 +0600

    handsonexercise

---
+++ codesamples/37_section35_36_review/03_handsonerxercise_02/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToTwentyOne/HonorableTodd_udemy_Golang_practice/codesamples/37_section35_36_review/03_handsonerxercise_02/quote"
+	"github.com/GoesToTwentyOne/HonorableTodd_udemy_Golang_practice/codesamples/37_section35_36_review/03_handsonerxercise_02/word"
+)
+
+func main() {
+	fmt.Println(word.Count(quote.SunAlso))
+
+	for k, v := range word.UseCount(quote.SunAlso) {
+		fmt.Println(v, k)
+	}
+}
