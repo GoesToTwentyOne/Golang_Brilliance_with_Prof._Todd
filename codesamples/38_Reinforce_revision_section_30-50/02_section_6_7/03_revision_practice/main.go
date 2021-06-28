1ad07bd86d1060c22da588bebee88cc9c5a87d8b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-29 02:25:47 +0600

    revision practice

---
+++ codesamples/38_Reinforce_revision_section_30-50/02_section_6_7/03_revision_practice/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const (
+	i int = 42
+	j     = "Nihad Hossain"
+)
+
+func main() {
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Printf("%T \n", i)
+	fmt.Printf("%T \n", j)
+
+}
