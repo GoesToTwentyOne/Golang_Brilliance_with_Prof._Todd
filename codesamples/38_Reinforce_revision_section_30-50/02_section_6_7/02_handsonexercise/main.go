06ddb5fb6c2e5523d5f5a497517c92de9783edbe
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-29 02:22:18 +0600

    revision section practice

---
+++ codesamples/38_Reinforce_revision_section_30-50/02_section_6_7/02_handsonexercise/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	h := (20 == 22)
+	i := (20 != 22)
+	j := (20 <= 22)
+	k := (20 >= 22)
+	l := (20 < 22)
+	m := (20 > 22)
+
+	fmt.Println(h)
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(l)
+	fmt.Println(m)
+
+}
