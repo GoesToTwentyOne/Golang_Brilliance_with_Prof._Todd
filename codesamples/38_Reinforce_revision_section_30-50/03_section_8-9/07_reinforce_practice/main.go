c41b99e1a8574cb4434608896d2b8cf85dbb741f
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-06-30 01:16:26 +0600

    reinforce practice

---
+++ codesamples/38_Reinforce_revision_section_30-50/03_section_8-9/07_reinforce_practice/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := "Protom Alo"
+	if x == "New York" {
+		fmt.Println("tre ", x)
+	} else if x == "bdnews24" {
+		fmt.Println("ture ", x)
+	} else if x == "Bangladeh Protidin" {
+		fmt.Println("ture ", x)
+	} else if x == "Times squre" {
+		fmt.Println("ture ", x)
+	} else if x == "Protom Alo" {
+		fmt.Println("ture ", x)
+	} else if x == "Dhaka Post" {
+		fmt.Println("ture ", x)
+	} else {
+		fmt.Println("all condition see off")
+	}
+
+}
