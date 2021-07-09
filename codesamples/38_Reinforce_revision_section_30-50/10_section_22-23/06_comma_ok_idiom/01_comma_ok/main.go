3def1545c4668adac2fc2b73ced402f0acc15b3e
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-09 13:38:09 +0600

    comma ok idom

---
+++ codesamples/38_Reinforce_revision_section_30-50/10_section_22-23/06_comma_ok_idiom/01_comma_ok/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	even := make(chan int)
+	odd := make(chan int)
+	quit := make(chan bool)
+	go send(even, odd, quit)
+	received(even, odd, quit)
+
+}
+func send(e, o chan<- int, q chan<- bool) {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			e <- i
+		} else {
+			o <- i
+		}
+	}
+	// close(q)
+	q <- true
+	close(o)
+	close(e)
+
+}
+func received(e, o <-chan int, q <-chan bool) {
+	for {
+		select {
+		case v := <-e:
+			fmt.Println("even ", v)
+		case v := <-o:
+			fmt.Println("odd  ", v)
+		case v, ok := <-q:
+			if !ok {
+				fmt.Println(v, ok)
+			} else {
+				fmt.Println(v, ok)
+
+			}
+			return
+
+		}
+	}
+
+}
