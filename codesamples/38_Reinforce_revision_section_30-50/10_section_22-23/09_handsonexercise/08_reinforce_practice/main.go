6f4105567c2733c759033fd7b08597ccf541dcb9
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-10 12:55:57 +0600

    reinforce practices and sort with order

---
+++ codesamples/38_Reinforce_revision_section_30-50/10_section_22-23/09_handsonexercise/08_reinforce_practice/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+	for i := 0; i < 10; i++ {
+
+		go func() {
+			for i := 0; i < 10; i++ {
+				c <- i
+
+			}
+		}()
+
+	}
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+}
