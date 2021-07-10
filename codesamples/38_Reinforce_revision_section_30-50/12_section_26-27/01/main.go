f2c6df10624eefd568050c8a896ab873bb8b7d63
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-10 10:41:04 +0600

    practice

---
+++ codesamples/38_Reinforce_revision_section_30-50/12_section_26-27/01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToTwentyOne/HonorableTodd_udemy_Golang_practice/codesamples/38_Reinforce_revision_section_30-50/12_section_26-27/01/mymath"
+)
+
+func main() {
+	fmt.Println("2 + 3 =", mymath.Sum(2, 3))
+	fmt.Println("4 + 7 =", mymath.Sum(4, 7))
+	fmt.Println("5 + 9 =", mymath.Sum(5, 9))
+}
