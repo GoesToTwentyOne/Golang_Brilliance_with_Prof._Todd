82e99c4f5c1e31b9d043ddee2d5c40c8653d55f5
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-11 00:44:01 +0600

    example test Implementation

---
+++ codesamples/27_Reinforce_revision_section_30-50/13_section_28-30/04_example_test/verocell/main.go
@@ -0,0 +1,9 @@
+package verocell
+
+func Add(xi ...int) int {
+	sum := 0
+	for _, v := range xi {
+		sum += v
+	}
+	return sum
+}
---
+++ codesamples/27_Reinforce_revision_section_30-50/13_section_28-30/04_example_test/verocell/main_test.go
@@ -0,0 +1,9 @@
+package verocell
+
+import "fmt"
+
+func ExampleAdd() {
+	fmt.Println(Add(45, 5, 6, 7, 8, 8))
+	//Output:
+	// 79
+}
