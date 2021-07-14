22f88c59bb71f64ac34407ceb213e66f82ce1edc
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-14 23:01:38 +0600

    reinforce practice

---
+++ codesamples/28_again_reinforce_practice_and_crystal_idea_all_topic/03_section_8-9_reinforce/08_handsonexercie/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch {
+	case (2 == 1):
+		fmt.Println("is there (2==1)     true or false |answers is ", (2 == 1))
+	case (4 == 5):
+		fmt.Println("is there (4==5)     true or false |answers is ", (4 == 5))
+	case (4 == 4):
+		fmt.Println("is there (4==4)     true or false |answers is ", (4 == 4))
+	default:
+		fmt.Println("everything is fair and lovely")
+	}
+
+}
