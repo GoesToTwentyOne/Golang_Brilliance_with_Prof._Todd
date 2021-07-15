2d773948afd0dda3deb6a4a17afab8f5a32ef491
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-15 11:19:50 +0600

    reinforce practice

---
+++ codesamples/28_again_reinforce_practice_and_crystal_idea_all_topic/04_section_10-11_reinforce_practice/10_handsonexercise/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	m["bill_kennedy"] = []string{"Hazelnut", "Grape", "Banana"}
+
+	for k, v := range m {
+		fmt.Println("Key is ", k)
+		for i, v := range v {
+			fmt.Println("\t", i, v)
+		}
+	}
+	fmt.Println("---------------------------------------------------------------------------------------")
+	delete(m, "bill_kennedy")
+	for k, v := range m {
+		fmt.Println("Key is ", k)
+		for i, v := range v {
+			fmt.Println("\t", i, v)
+		}
+	}
+
+}
