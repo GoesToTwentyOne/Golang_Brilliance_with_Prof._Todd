fa1a7566d1a802ccd9a8c323fa80268d9a02d750
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-06-13 16:23:54 +0600

    exploring data type

---
+++ codesamples/39_reinforce_revision_3rd_times/01_section_4/06_exploring_type/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x = 10
+var y = "Niahd"
+
+func main() {
+	fmt.Printf("%v\n", x)
+	fmt.Printf("%T\n", x)
+	fmt.Printf("%v\n", y)
+	fmt.Printf("%T\n", y)
+	//go is all about type
+
+}
