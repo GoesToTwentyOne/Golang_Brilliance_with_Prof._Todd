349fa5927a0084d1e129d6eb216920a0156ae6e1
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-06-13 18:55:58 +0600

    handson

---
+++ codesamples/39_reinforce_revision_3rd_times/01_section_4/12_handson_exercise/05/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type cat int
+
+var x cat
+var y int
+
+func main() {
+	fmt.Printf("%v\n", x)
+	fmt.Printf("%T\n", x)
+	x = 42
+	fmt.Printf("%v\n", x)
+	y = int(x)
+	fmt.Printf("%v\n", y)
+	fmt.Printf("%T\n", y)
+
+}
