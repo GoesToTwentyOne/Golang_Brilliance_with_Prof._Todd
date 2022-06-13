1bd7829805c4d556a0c7bc80da1817a47d9b405b
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-06-13 18:32:16 +0600

    handson exercise

---
+++ codesamples/39_reinforce_revision_3rd_times/01_section_4/12_handson_exercise/01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	b := "James Bond"
+	c := true
+	fmt.Printf("%v\n", a)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%v\n", c)
+	fmt.Printf("%v \t %v \t %v\n", a, b, c)
+
+}
