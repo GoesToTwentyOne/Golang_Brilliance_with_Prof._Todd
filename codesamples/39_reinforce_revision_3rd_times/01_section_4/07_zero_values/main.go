85f3d4737dc2129b51899541f0698bdb88192d15
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-06-13 16:36:20 +0600

    zero values

---
+++ codesamples/39_reinforce_revision_3rd_times/01_section_4/07_zero_values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z float64
+
+func main() {
+	fmt.Printf("value of x is: %v\n", x)
+	fmt.Printf("Type of x is: %T\n", x)
+	fmt.Printf("value of y is: %v\n", y)
+	fmt.Printf("type of y is: %T\n", y)
+	fmt.Printf("value of z is: %v\n", z)
+	fmt.Printf("type of z is: %T\n", z)
+
+}
