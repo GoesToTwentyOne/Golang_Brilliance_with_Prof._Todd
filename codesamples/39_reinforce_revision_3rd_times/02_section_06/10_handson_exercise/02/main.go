805ec66f27ab5c4e48e90bdf5cc1aea022cae4db
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-06-14 18:56:01 +0600

    handson

---
+++ codesamples/39_reinforce_revision_3rd_times/02_section_06/10_handson_exercise/02/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := (200 == 2004)
+	b := (200 <= 2004)
+	c := (200 >= 2004)
+	d := (200 != 2004)
+	e := (200 > 2004)
+	f := (200 < 2004)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+
+}
