f0ed3dc3dd46f2e7b2fd25bcc55fd51037b67c0e
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-06-18 11:36:52 +0600

    handson

---
+++ codesamples/39_reinforce_revision_3rd_times/03_section_08/15_handson/06/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 2
+	if true {
+		fmt.Println("true")
+	}
+	if 2 == 2 {
+		fmt.Println("true")
+	}
+	if 2 != 2 {
+		fmt.Println("false")
+	}
+	if x == 2 {
+		fmt.Println("2")
+
+	}
+
+}
