acd346f4c2563923fbdd850aa45d8c76d163f6c4
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2022-07-03 07:48:34 +0600

    race con   handson

---
+++ codesamples/39_reinforce_revision_3rd_times/09_section_20/06_handson/04/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	counter := 0
+	fmt.Println("CPUs :", runtime.NumCPU())
+	fmt.Println("Go Routine :", runtime.NumGoroutine())
+	var wg sync.WaitGroup
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			v := counter
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+	}
+	fmt.Println(counter)
+	fmt.Println("CPUs :", runtime.NumCPU())
+	fmt.Println("Go Routine :", runtime.NumGoroutine())
+	wg.Wait()
+}
