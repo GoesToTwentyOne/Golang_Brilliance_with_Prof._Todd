2c7b65cc9e1031499f54ddae17ec33d3d0734208
GoesToTwentyOne <77912479+GoesToTwentyOne@users.noreply.github.com>
2023-02-14 15:30:01 +0600

    type/untype constant

---
+++ codesamples/41_Final_time_rivision/06_section_06_programming_fundamentals/03_constant/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a = 20
+	b = 3.9
+)
+const (
+	x int  = 34
+	y bool = true
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(x)
+	fmt.Println(y)
+
+}
