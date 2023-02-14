84ea814cdcfc220696f913c7d133b59493cb36b0
GoesToTwentyOne <77912479+GoesToTwentyOne@users.noreply.github.com>
2023-02-14 21:17:56 +0600

    if statement

---
+++ codesamples/41_Final_time_rivision/08_section_08_control_flow/06_if_statements/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	if x := 23; x == 2 {
+		fmt.Println("you are doing great")
+	}
+	if x := 2; x == 2 {
+		fmt.Println("You are doing great")
+	}
+}
