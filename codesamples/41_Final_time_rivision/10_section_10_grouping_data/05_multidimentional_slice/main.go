5dbd005bb01d8cba2386880f6acb1133409678e0
GoesToTwentyOne <77912479+GoesToTwentyOne@users.noreply.github.com>
2023-02-15 03:57:24 +0600

    multidimentional slice

---
+++ codesamples/41_Final_time_rivision/10_section_10_grouping_data/05_multidimentional_slice/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := [][]string{
+		{"Md.Nihad Hossain", "Alex Goot", "Jytarino"},
+		{"Hexna", "Robsin", "Haronyto"},
+	}
+	fmt.Println(x)
+}
